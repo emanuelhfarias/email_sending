a9ceee98fff35175520923158f1b76275edaa08b
Emanuel H. Farias <emanuelfarias@outlook.com>
2020-03-15 20:24:04 -0300

    add Email model on django-admin
    $ ./manage makemigrations

--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Email(models.Model):
+    PENDING = 'Pending'
+    FAILED = 'Failed'
+    SENT = 'Sent'
+    STATUS_CHOICES = [
+      (PENDING, PENDING.capitalize()),
+      (FAILED, FAILED.capitalize()),
+      (SENT, SENT.capitalize()),
+    ]
+
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    subject = models.CharField(max_length=256, null=False, blank=False)
+    origin = models.EmailField(verbose_name='From')
+    destination = models.TextField(
+      null=False, blank=False, verbose_name='To', help_text="Comma separated emails: x@test.com, y@test.com")
+    message = models.TextField(null=False, blank=False)
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default=PENDING, editable=False)
