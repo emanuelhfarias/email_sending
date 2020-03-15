af883a72fff1dc0225683ed28cc7284341d3e348
Emanuel H. Farias <emanuelfarias@outlook.com>
2020-03-15 20:26:49 -0300

    customize email form

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
+from django.db import models
+from django.forms import Textarea
 
 from .models import Email
 
-admin.site.register(Email)
+class EmailAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'origin', 'destination', 'status')
+
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(attrs={'rows': 2})},
+    }
+
+admin.site.register(Email, EmailAdmin)
