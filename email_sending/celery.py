92f24fe879a954d6a8840d8373ececa341539b7b
Emanuel H. Farias <emanuelfarias@outlook.com>
2020-03-15 21:51:59 -0300

    reconfigure Celery in core app

--- email_sending/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'email_sending.settings')
-
-app = Celery('email_sending')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
